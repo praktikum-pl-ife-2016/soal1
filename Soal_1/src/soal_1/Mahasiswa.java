017f2505b07011cdbb6f3ed2719eacd583e1f64b
A_FRDS <agung.frds7@gmail.com>
2016-03-02 23:22:10 +0700

    Edit kelas Mahasiswa dan Main

--- Soal_1/src/soal_1/Mahasiswa.java
+++ Soal_1/src/soal_1/Mahasiswa.java
@@ -1,5 +1,32 @@
-package soal_1;
-
 public class Mahasiswa {
-    //isi
-}
+    public String nama;
+    public String nim;
+    public String prodi;
+    public String kelas;
+    public String ang;
+    
+    public void setNama (String a) {
+        nama = a;
+    }
+    
+    public void setNim (String a) {
+        nim = a;
+    }
+    public void setProdi (String a) {
+        prodi = a;
+    }
+    public void setKelas (String a) {
+        kelas = a;
+    }
+    public void setAng (String a) {
+        ang = a;
+    }
+        
+    public void Display(){
+        System.out.println("Nama\t\t:" + nama);
+        System.out.println("NIM\t\t:" + nim);
+        System.out.println("Program Studi\t:" + prodi);
+        System.out.println("Angkatan\t:" + ang);
+        System.out.println("Kelas\t\t:" + kelas);   
+    }
+    }
--- Soal_1/src/soal_1/Main.java
+++ Soal_1/src/soal_1/Main.java
@@ -1,9 +1,32 @@
-package soal_1;
-
 public class Main {
-
-    public static void main(String[] args) {
-        //isi
-    }
     
+    public static void main(String[] args) {
+        Mahasiswa m1 = new Mahasiswa();
+        m1.setNama("Agung Firdaus");
+        m1.setNim("155150200111054");
+        m1.setProdi("Teknik Informatika");
+        m1.setAng("2015");
+        m1.setKelas("TIF-E");
+        m1.Display();
+        System.out.println("====================================");
+        
+        Mahasiswa m2 = new Mahasiswa();
+        m2.setNama("Vasha Farizi");
+        m2.setNim("155150200111053");
+        m2.setProdi("Teknik Informatika");
+        m2.setAng("2015");
+        m2.setKelas("TIF-D");
+        m2.Display();
+        System.out.println("====================================");
+        
+        Mahasiswa m3 = new Mahasiswa();
+        m3.setNama("Willy Aditya Pratama");
+        m3.setNim("155150200111264");
+        m3.setProdi("Sistem Informasi");
+        m3.setAng("2015");
+        m3.setKelas("SI-E");
+        m3.Display();
+        System.out.println("====================================");
+    }
+
 }
