40c763f308365e768c2d87174958edcf977ff40b
rizkiramadhan2 <rizki2500@gmail.com>
2016-03-02 21:55:48 +0700

    edit class Mahasiswa

--- Soal_1/src/soal_1/Mahasiswa.java
+++ Soal_1/src/soal_1/Mahasiswa.java
@@ -1,5 +1,32 @@
 package soal_1;
 
 public class Mahasiswa {
-    //isi
+    private String nama;
+    private String nim;
+    private String kelas;
+    private String angkatan;
+    private String noHp;
+    Mahasiswa(){}
+    public void setNama(String s){
+        nama = s;
+    }
+    public void setNim(String s){
+        nim = s;
+    }
+    public void setKelas(String s){
+        kelas = s;
+    }
+    public void setAngkatan(String s){
+        angkatan = s;
+    }
+    public void setNoHp(String s){
+        noHp = s;
+    }
+    public void display(){
+        System.out.println("Nama : "+nama);
+        System.out.println("NIM  : "+nim);
+        System.out.println("Kelas :" +kelas);
+        System.out.println("Angkatan : "+ angkatan);
+        System.out.println("No HP : "+ noHp);
+    }
 }
