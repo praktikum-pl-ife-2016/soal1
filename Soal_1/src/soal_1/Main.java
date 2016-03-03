20638269937f9e406180623aef490bc5ab7d5bc6
albertbill <albertbillalroy@gmail.com>
2016-03-03 11:50:54 +0700

    Main.java edited

--- Soal_1/src/soal_1/Main.java
+++ Soal_1/src/soal_1/Main.java
@@ -3,7 +3,33 @@ package soal_1;
 public class Main {
 
     public static void main(String[] args) {
-        //isi
+        Mahasiswa m1 = new Mahasiswa();
+        m1.setNama("Albert Bill Alroy");
+        m1.setNim("155150201111087");
+        m1.setAbsen(28);
+        m1.setKelas("TIF - E");
+        m1.setAngkatan(2015);
+        m1.displayMessage();
+        System.out.println("=====================");
+        
+        Mahasiswa m2 = new Mahasiswa();
+        m2.setNama("Wunsel Arto Negoro");
+        m2.setNim("155150201111088");
+        m2.setAbsen(29);
+        m2.setKelas("TIF - E");
+        m2.setAngkatan(2015);
+        m2.displayMessage();
+        System.out.println("=====================");
+        
+        Mahasiswa m3 = new Mahasiswa();
+        m3.setNama("Lutfi Pratama");
+        m3.setNim("155150201111089");
+        m3.setAbsen(30);
+        m3.setKelas("TIF - E");
+        m3.setAngkatan(2015);
+        m3.displayMessage();
+        System.out.println("=====================");
+    
     }
     
 }
