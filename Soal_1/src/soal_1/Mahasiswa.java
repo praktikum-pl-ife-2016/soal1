2fa9ec7e6ba433ab5606f9a49807f067ec7db17e
adikurniawan1996 <adikurniawan1996@gmail.com>
2016-03-03 12:47:38 +0700

    mengedit sourcecode

--- Soal_1/src/soal_1/Mahasiswa.java
+++ Soal_1/src/soal_1/Mahasiswa.java
@@ -1,5 +1,47 @@
-package soal_1;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package siam;
 
+/**
+ *
+ * @author guest-tyhykG
+ */
 public class Mahasiswa {
-    //isi
+
+    public String nama;
+    public String nim;
+    public String kelas;
+    public int umur;
+    public String fakultas;
+
+    public void setNama(String n) {
+        nama = n;
+    }
+
+    public void setNim(String NIM) {
+        nim = NIM;
+    }
+
+    public void setKelas(String Kelas) {
+        kelas = Kelas;
+    }
+
+    public void setUmur(int usia) {
+        umur = usia;
+    }
+
+    public void setFakultas(String fak) {
+        fakultas = fak;
+    }
+
+    public void tampilan() {
+        System.out.println("Nama : " + nama);
+        System.out.println("umur : " + umur);
+        System.out.println("Kelas : " + kelas);
+        System.out.println("NIM : " + nim);
+        System.out.println("Fakultas : " + fakultas);
+    }
 }
