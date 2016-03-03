d3232a4891350c3c4dcae59add955918ab7a9cdc
Rezananda <email github>
2016-03-03 11:55:23 +0700

    Ubah kode

--- Soal_1/src/soal_1/Mahasiswa.java
+++ Soal_1/src/soal_1/Mahasiswa.java
@@ -1,5 +1,52 @@
-package soal_1;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mahasiswa;
 
+/**
+ *
+ * @author guest-xrNNQ7
+ */
 public class Mahasiswa {
-    //isi
+
+    private String nama;
+    private int nim;
+    private String kelas;
+    private int semester;
+    private String universitas;
+
+    /**
+     * @param args the command line arguments
+     */
+
+    public void setNama(String s) {
+        nama = s;
+    }
+
+    public void setNim(int i) {
+        nim = i;
+    }
+
+    public void setKelas(String s) {
+        kelas = s;
+    }
+
+    public void setSemester(int i) {
+        semester = i;
+    }
+
+    public void setUniversitas(String s) {
+        universitas = s;
+    }
+
+    public void displayMessage() {
+        System.out.println("Nama saya adalah " + nama);
+        System.out.println("NIM saya adalah " + nim);
+        System.out.println("Kelas saya adalah " + kelas);
+        System.out.println("Saya semester " + semester);
+        System.out.println("Saya kuliah di " + universitas);
+    }
+
 }
