0937fbd4140e1aa19ca0f3ababef6b12dc749af0
rasioatmaja29 <rasioatmaja29@gmail.com>
2016-03-03 11:51:26 +0700

    Source code

--- Soal_1/src/soal_1/Mahasiswa.java
+++ Soal_1/src/soal_1/Mahasiswa.java
@@ -1,5 +1,39 @@
 package soal_1;
-
 public class Mahasiswa {
-    //isi
+
+    public String nama;
+    public String nim;
+    public String fakultas;
+    public String jurusan;
+    public int angkatan;
+
+    public void setNama(String s) {
+        nama = s;
+    }
+
+    public void setnim(String s) {
+        nim = s;
+    }
+
+    public void setfakultas(String s) {
+        fakultas = s;
+    }
+
+    public void setjurusan(String s) {
+
+        jurusan = s;
+    }
+
+    public void setangkatan(int i) {
+
+        angkatan = i;
+    }
+
+    public void displayMessage() {
+        System.out.println("Nama     :" + nama);
+        System.out.println("NIM      :" + nim);
+        System.out.println("Fakultas :" + fakultas);
+        System.out.println("Jurusan  :" + jurusan);
+        System.out.println("Angkatan :" + angkatan);
+    }
 }
