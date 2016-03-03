91616ac4f202589a0e4df490447f9697de582244
Febriana <rantalidya18@gmail.com>
2016-03-03 07:59:20 +0700

    Membuat class dan object

---
+++ Soal_1/src/soal_1/mainSoalNo1.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SoalNo1;
+
+/**
+ *
+ * @author guest-OozuVs
+ */
+public class mainSoalNo1 {
+    public static void main(String[] args) {
+        //instan objek bernama n1
+        SoalNo1 n1 = new SoalNo1();
+        n1.setnama("febriana");
+        n1.setnim("155150207111037");
+        n1.setalamat("Suhat");
+        n1.setusia("19");
+        n1.setip(3.22);
+        n1.displayMessage();
+        System.out.println("==========================");
+        //instan objek bernama n2
+        SoalNo1 n2 = new SoalNo1();
+        n2.setnama("jalal");
+        n2.setnim("155150207000897");
+        n2.setalamat("malang");
+        n2.setusia("20");
+        n2.setip(2.95);
+        n2.displayMessage();
+        System.out.println("===========================");
+        //instan objek bernama n3
+        SoalNo1 n3 = new SoalNo1();
+        n3.setnama("jakatingkir");
+        n3.setnim("155150207000456");
+        n3.setalamat("Sumbersari");
+        n3.setusia("18");
+        n3.setip(3.56);
+        n3.displayMessage();
+        System.out.println("============================");
+        
+    }
+    
+}
