f8e4cd0f81fdefb0c01d5d8d6d1bea9fb6a9a306
TerryKincowati <terrykincowati.tk@gmail.com>
2016-03-03 06:26:37 +0000

    Soure Code

--- Soal_1/src/soal_1/Mahasiswa.java
+++ Soal_1/src/soal_1/Mahasiswa.java
@@ -1,5 +1,47 @@
-package soal_1;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Soal_1;
 
-public class Mahasiswa {
-    //isi
-}
+
+public class Mahasiswa {   
+    private String namaMahasiswa;
+    private int noAbsen;
+    private String nim;
+    private int usia;
+    private String mataKuliah;
+    
+    public void setNamaMahasiswa(String s) {
+        namaMahasiswa = s;
+    }
+    
+    public void setNoAbsen(int i) {
+        noAbsen = i;
+    }
+
+    public void setNIM(String s) {
+        nim = s;
+    }
+
+    public void setUsia(int i) {
+        usia = i;
+    }
+
+    public void setMataKuliah(String s) {
+        mataKuliah = s;
+    }
+
+    public void displayMessage() {
+        System.out.println("Nama\t\t: " +namaMahasiswa);
+        System.out.println("Nim\t\t: " +nim);
+        System.out.println("No Absen\t: " +noAbsen);
+        System.out.println("Usia\t\t: "+usia);
+        System.out.println("Mata kuliah\t: " +mataKuliah);
+    }
+    
+    //Tidak usah dikasih main method
+    //main method hanya pada mainClass
+    
+    }
--- Soal_1/src/soal_1/Main.java
+++ Soal_1/src/soal_1/Main.java
@@ -1,9 +1,38 @@
-package soal_1;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Soal_1;
+
 
 public class Main {
-
     public static void main(String[] args) {
-        //isi
+        Mahasiswa s1 = new Mahasiswa(); 
+        s1.setNamaMahasiswa("Teri Kincowati");
+        s1.setNoAbsen(22);
+        s1.setNIM("155150201111082");
+        s1.setUsia(19);
+        s1.setMataKuliah("Pemrogaman Lanjutan");
+        s1.displayMessage();
+        System.out.println("======================================");
+        
+        Mahasiswa s2 = new Mahasiswa(); 
+        s2.setNamaMahasiswa("Irma Nurvianti");
+        s2.setNoAbsen(24);
+        s2.setNIM("155150201111084");
+        s2.setUsia(18);
+        s2.setMataKuliah("Pemrogaman Lanjutan");
+        s2.displayMessage();
+        System.out.println("======================================");
+                  
+        Mahasiswa s3 = new Mahasiswa(); 
+        s3.setNamaMahasiswa("Riska Andriani");
+        s3.setNoAbsen(20);
+        s3.setNIM("155150201111080");
+        s3.setUsia(19);
+        s3.setMataKuliah("Pemrogaman Lanjutan");
+        s3.displayMessage();
+        System.out.println("======================================");
     }
-    
 }
