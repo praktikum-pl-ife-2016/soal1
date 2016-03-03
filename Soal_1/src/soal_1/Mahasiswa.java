89ea30e5b53aa90c72c04d5433706627a83e6f69
iwisuharti <iwisuharti@gmail.com>
2016-03-03 07:00:49 +0000

    Source Code"

--- Soal_1/src/soal_1/Mahasiswa.java
+++ Soal_1/src/soal_1/Mahasiswa.java
@@ -1,5 +1,27 @@
-package soal_1;
+package tugasasprak;
 
-public class Mahasiswa {
-    //isi
+public class mahasiswa {
+public String nama;
+public String nim;
+public String kelas;
+public String fakultas;
+public String jurusan;
+public void setnama(String s){
+nama = s;
 }
+public void setnim(String s){
+nim = s;
+}
+public void setkelas(String s){
+kelas = s;
+}
+public void setfakultas(String i){fakultas = i;
+}
+public void setjurusan (String s){ jurusan = s;};
+public void displayMessage(){
+System.out.println("Nama anda adalah " + nama);
+System.out.println("NIM anda " + nim);
+System.out.println("Anda adalah anggota kelas " + kelas);
+System.out.println("Fakultas " + fakultas);
+System.out.println("Jurusan " + jurusan);
+}}
