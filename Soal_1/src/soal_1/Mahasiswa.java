f4c106a856d708e4bef098926569b44deede657c
Affan@Alvyan <affanalvyan@yahoo.co.id>
2016-03-05 00:34:52 +0700

    Edit file java dan data diri

--- Soal_1/src/soal_1/Mahasiswa.java
+++ Soal_1/src/soal_1/Mahasiswa.java
@@ -1,5 +1,29 @@
-package soal_1;
-
+package mahasiswa;
 public class Mahasiswa {
-    //isi
+    private String nama, nim, hobi;
+    private int tinggi, berat;
+    
+    public void setNama (String s) {
+        nama = s;
+    }
+    public void setNim (String s) {
+        nim = s;
+    }
+    public void setTinggi (int i) {
+        tinggi = i;
+    }
+    public void setBerat (int i) {
+        berat = i;
+    }
+    public void setHobi (String s) {
+        hobi = s;
+    }
+    public void displayMessage() {
+        System.out.println("Nama Anda adalah "+nama);
+        System.out.println("Anda memiliki NIM "+nim);
+        System.out.println("Tinggi badan Anda "+tinggi+" cm");
+        System.out.println("Berat badan Anda "+berat+" kg");
+        System.out.println("Hobi Anda "+hobi);
+    }
+    
 }
