13e7a390a9dd269ab26b9f37cc68d69b3b8326ff
Fikri <fikriihsan003@yahoo.com>
2016-03-04 23:46:54 +0700

    Memasukan source code

--- Soal_1/src/soal_1/Mahasiswa.java
+++ Soal_1/src/soal_1/Mahasiswa.java
@@ -1,5 +1,15 @@
 package soal_1;
 
 public class Mahasiswa {
-    //isi
+     public  String nama="ihsan";
+    public  int nim=1551502071;
+    public  int umur=19;
+    public  String jurusan="informatika";
+    public String makanKesukaan="bubur";
+    public Mahasiswa(){
+        
+    }
+ public  void getData(){
+     System.out.println(nama+" "+nim+" "+" "+umur+" "+jurusan+" "+makanKesukaan);
+ }
 }
--- Soal_1/src/soal_1/Main.java
+++ Soal_1/src/soal_1/Main.java
@@ -3,7 +3,24 @@ package soal_1;
 public class Main {
 
     public static void main(String[] args) {
-        //isi
+        mahasiswa satu=new mahasiswa();
+        satu.getData();
+        mahasiswa dua=new mahasiswa();
+        dua.jurusan="siskom";
+        dua.makanKesukaan="lele";
+        dua.nama="ahmad";
+        dua.nim=3124235;
+        dua.umur=12;
+        dua.getData();
+        mahasiswa tiga=new mahasiswa();
+         tiga.jurusan="agrotek";
+        tiga.makanKesukaan="tahu";
+        tiga.nama="nopal";
+        tiga.nim=1221211;
+        tiga.umur=13;
+        tiga.getData();
+            
+
     }
     
 }
