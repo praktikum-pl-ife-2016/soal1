9825715c572790e48c52484b015bb6f0589322f6
A_FRDS <agung.frds7@gmail.com>
2016-03-06 01:32:56 +0700

    Source code

--- Soal_1/src/soal_1/Mahasiswa.java
+++ Soal_1/src/soal_1/Mahasiswa.java
@@ -1,5 +1,38 @@
-package soal_1;
+package latianyau;
 
 public class Mahasiswa {
-    //isi
+    private String nama;
+    private String nim;
+    private double IP;
+    private String merkhape;
+    private int jajansebulan;
+    
+    public void setnama(String s){
+        nama = s;
+    }
+    public void setnim(String a){
+        nim = a;
+    }
+    public void setIP(double b){
+        IP = b;
+    }
+    public void setmerkhape(String c){
+        merkhape = c;
+    }
+    public void setjajansebulan(int d){
+        jajansebulan = d;
+    }
+    public void Hasil(){
+ System.out.println("Nama anda adalah "+nama);
+ System.out.println("mempunyai nomor nim "+nim);
+ System.out.println("serta memililki IP "+IP);
+ System.out.println("dan memiliki hape "+merkhape);
+ System.out.println("Jajan anda sebulan adalah "+jajansebulan);
+ 
+
+   
+        
+    }
 }
+    
+
---
+++ Soal_1/src/soal_1/MainMahasiswa.java
@@ -0,0 +1,38 @@
+package latianyau;
+
+public class MainMahasiswa {
+       public static void main(String[] args) {
+ //instan objek bernama m1
+ Mahasiswa m1 = new Mahasiswa();
+ m1.setnama("Rizki Ramadhan Aditya Sholeh");
+ m1.setnim("155150200111059");
+ m1.setIP(3.5);
+ m1.setmerkhape("Oppo");
+ m1.setjajansebulan(1700000);
+ m1.Hasil();
+ System.out.println("================");
+ //instan objek baru bernama m2
+ Mahasiswa m2 = new Mahasiswa();
+ m2.setnama("Mohamad Mahendra Saifulah");
+ m2.setnim("155150207111034");
+ m2.setIP(3.6);
+ m2.setmerkhape("LG");
+ m2.setjajansebulan(1200000);
+ m2.Hasil();
+ System.out.println("================");
+ 
+ Mahasiswa m3 = new Mahasiswa();
+ m3.setnama("Faisal Ainun Najib ");
+ m3.setnim("155150200111063");
+ m3.setIP(3.7);
+ m3.setmerkhape("Lenovo");
+ m3.setjajansebulan(5000);
+ m3.Hasil();
+ System.out.println("================");
+
+ 
+ }
+}
+    
+    
+
