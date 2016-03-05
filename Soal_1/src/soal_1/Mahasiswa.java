fb8bdb6a89b25775f87365b447ff822179bf2a90
muhammadainunnajib <ainunnajib02@yahoo.com>
2016-03-05 23:17:08 +0700

    latihan praktikum

--- Soal_1/src/soal_1/Mahasiswa.java
+++ Soal_1/src/soal_1/Mahasiswa.java
@@ -1,5 +1,41 @@
-package soal_1;
 
 public class Mahasiswa {
-    //isi
+    public String nama;
+    public String nim;
+    public String email;
+    public String telp;
+    public String tglLahir;
+    
+    public String setNama (String a){
+        nama = a;
+        return nama;
+    }
+    
+    public String setNim (String a){
+        nim = a;
+        return nim;
+    }
+    
+    public String setEmail (String a){
+        email = a;
+        return email;
+    }
+    
+    public String setTelp (String a){
+        telp = a;
+        return telp;
+    }
+    
+    public String setTgl (String a){
+        tglLahir = a;
+        return tglLahir;
+    }
+    
+    public void TampilData(){
+        System.out.println("Nama anda : " + nama);
+        System.out.println("Nim anda : " + nim);
+        System.out.println("Email anda : " + email);
+        System.out.println("Nomor Telepon anda : " + telp);
+        System.out.println("Tanggal Lahir anda : " + tglLahir);
+    }
 }
--- Soal_1/src/soal_1/Main.java
+++ Soal_1/src/soal_1/Main.java
@@ -1,9 +1,29 @@
-package soal_1;
 
 public class Main {
-
-    public static void main(String[] args) {
-        //isi
+    public static void main(String[] args){
+        Mahasiswa Data1 = new Mahasiswa();
+        Data1.setNama("Yuri");
+        Data1.setNim("155150200111061");
+        Data1.setEmail("Yuri@yahoo.com");
+        Data1.setTgl("15-02-1998");
+        Data1.setTelp("085704326826");
+        Data1.TampilData();
+        System.out.println("-----------------");
+        Mahasiswa Data2 = new Mahasiswa();
+        Data2.setNama("Soap");
+        Data2.setNim("155150200111062");
+        Data2.setEmail("Soap@yahoo.com");
+        Data2.setTgl("20-01-1997");
+        Data2.setTelp("087876549873");
+        Data2.TampilData();
+        System.out.println("-----------------");
+        Mahasiswa Data3 = new Mahasiswa();
+        Data3.setNama("Frost");
+        Data3.setNim("155150200111063");
+        Data3.setEmail("Frost@yahoo.com");
+        Data3.setTgl("30-12-1996");
+        Data3.setTelp("081871651091");
+        Data3.TampilData();
+        System.out.println("-----------------");
     }
-    
 }
