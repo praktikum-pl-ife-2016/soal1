8690a719009972a75bc35b3ce2f674eb7b8f2f61
andika22 <andikaisranugraha97@gmail.com>
2016-03-05 16:20:22 +0700

    Added files via upload
    TUGAS

---
+++ MainMahasiswa.java
@@ -0,0 +1,34 @@
+
+package mahasiswa;
+
+
+public class MainMahasiswa {
+    public static void main(String[] args) {
+        Mahasiswa m1 = new Mahasiswa();
+        m1.setnama("Rhomzy");
+        m1.setnim(1551542);
+        m1.setalamat("Malang");
+        m1.setjurusan("Teknik Informatika");
+        m1.setkelas("E");
+        m1.displaymessage();
+        System.out.println("==================================");
+        Mahasiswa m2 = new Mahasiswa();
+        m2.setnama("X-Silent");
+        m2.setnim(1551576);
+        m2.setalamat("Jakarta");
+        m2.setjurusan("Teknik Informatika");
+        m2.setkelas("E");
+        m2.displaymessage();
+        System.out.println("==================================");
+        Mahasiswa m3 = new Mahasiswa();
+        m3.setnama("Riza");
+        m3.setnim(1551502);
+        m3.setalamat("Tulung Agung");
+        m3.setjurusan("Teknik Informatika");
+        m3.setkelas("E");
+        m3.displaymessage();
+        System.out.println("==================================");
+        }
+    }
+    
+
