cab42114395e41de480d9b46d092a97d4adc1276
RizkiFauzi <rizqif.axe@gmail.com>
2016-03-05 23:58:06 +0700

    praktikum 1

--- Soal_1/src/soal_1/Mahasiswa.java
+++ Soal_1/src/soal_1/Mahasiswa.java
@@ -1,5 +1,30 @@
-package soal_1;
-
 public class Mahasiswa {
-    //isi
+    private String nama;
+    private String nim;
+    private String kelas;
+    private int angkatan;
+    private String asal;
+    
+    public void setnama(String s){
+        nama = s;
+    }
+    public void setnim(String a){
+        nim = a;
+    }
+    public void setkelas(String b){
+        kelas = b;
+    }
+    public void setasal(String c){
+        asal = c;
+    }
+    public void setangkatan(int d){
+        angkatan = d;
+    }
+    public void Tampilan(){
+ System.out.println("Nama anda adalah "+nama);
+ System.out.println("Nim anda adalah "+nim);
+ System.out.println("kelas anda "+ kelas);
+ System.out.println("Angkatan anda adalah "+angkatan);
+ System.out.println("Asal daerah anda adalah "+asal);   
+    }
 }
