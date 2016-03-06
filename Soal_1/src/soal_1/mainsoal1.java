3c20d9fe85a25a6460ebc87812495d8077e8e40a
melati-al <imel_mail@rocketmail.com>
2016-03-07 00:00:09 +0700

    Mengubah kelas

---
+++ Soal_1/src/soal_1/mainsoal1.java
@@ -0,0 +1,42 @@
+package soal1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mahasiswa
+ */
+public class mainsoal1 {
+    public static void main(String[] args) {
+        Soal1 s1  = new Soal1();
+        s1.setNama("Melati Ayuning Lestari");
+        s1.setNim("155150207111039");
+        s1.setAsal("Depok");
+        s1.setUmur(18);
+        s1.setJurusan("filkom");
+        s1.displaymessage();
+        System.out.println("============================");
+        
+        Soal1 s2  = new Soal1();
+        s2.setNama("Febriayana Lidya");
+        s2.setNim("155150207111037");
+        s2.setAsal("Medan");
+        s2.setUmur(18);
+        s2.setJurusan("filkom");
+        s2.displaymessage();
+        System.out.println("============================");
+        
+        Soal1 s3  = new Soal1();
+        s3.setNama("Fatima");
+        s3.setNim("155150207111039");
+        s3.setAsal("Lumajang");
+        s3.setUmur(18);
+        s3.setJurusan("filkom");
+        s3.displaymessage();
+        System.out.println("============================");
+    }
+}
